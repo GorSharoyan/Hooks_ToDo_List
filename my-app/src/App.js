da7ceff77ff812ecd7d582e04a7a2aae2aa7fb5e
Gor <gor.sharoyan95@gmail.com>
2020-09-16 02:02:51 +0400

    started experimenting with hooks

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback, useMemo } from "react";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [tods,setTodos]=useState([]);
+  const [todoInput,setTodoInput]=useState('');
+
+  // const handleTodoInput=({target:vale})=>{
+  //   setTodoInput(value)
+  // }
+  // const handleTodoUpdate=()={
+  //   setTodos({
+
+  //   })
+  // }
+
+  return(
+    <div>
+      <ul>
+        <input/>
+        <button>Add</button>
+      </ul>
     </div>
-  );
+  )
 }
+
 
 export default App;
